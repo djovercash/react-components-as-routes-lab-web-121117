b0b2a618e3cae53b7ef0d55049abf3e0e3757bc3
github username <github email address>
2018-02-26 14:01:35 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h2>Name: {movie.title}</h2>
+            <h3>Time: {movie.time}</h3>
+            <h3>Genres: </h3>
+            <ul>
+            {movie.genres.map(genre => {
+              return (
+                <li key={genre}>{genre}</li>
+              )
+            })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  background: 'blue'
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to='/' exact style={{link}} activeStyle={{background: 'dark blue'}}>Home</NavLink><br />
+      <NavLink to='/movies' exact style={{link}} activeStyle={{background: 'dark blue'}}>Movies</NavLink><br />
+      <NavLink to='/directors' exact style={{link}} activeStyle={{background: 'dark blue'}}>Directors</NavLink><br />
+      <NavLink to='/actors' exact style={{link}} activeStyle={{background: 'dark blue'}}>Actors</NavLink>
     </div>
   );
 };
